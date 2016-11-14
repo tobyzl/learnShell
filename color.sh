17fa811f026f438e2051a3918f3e4c795c6c9620
toby.zhang <toby.zhang@padsystem.com>
2016-11-14 14:46:44 +0800

    add color.sh

---
+++ color.sh
@@ -0,0 +1,3 @@
+echo "\033[32m green \033[0m"
+
+read -p "Press any key to continue." 
--- hello.sh
+++ hello.sh
@@ -1,3 +1,7 @@
-echo hello
+#!/bin/bash
+
+#"#!" 是一个约定的标记，它告诉系统这个脚本需要什么解释器来执行，即使用哪一种Shell。
+#echo命令用于向窗口输出文本。 
+echo "hello"
 
 read -p "Press any key to continue."                   
